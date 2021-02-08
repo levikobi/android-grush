f3b335fca30f550d182047d3cbefc37e031dcf85
kobi levi <kobilevi503@gmail.com>
2021-02-08 19:07:22 +0200

    Dummy products RecyclerView

--- app/src/main/java/com/comas/grush/ui/home/HomeFragment.java
+++ app/src/main/java/com/comas/grush/ui/home/HomeFragment.java
@@ -11,23 +11,46 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.comas.grush.R;
+import com.comas.grush.adapters.ProductListAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView mRecyclerView;
+    private ProductListAdapter mAdapter;
+
+    private final List<String> mProductList = new LinkedList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
+
+        for (int i = 0; i < 20; i++) {
+            mProductList.add("Word " + i);
+        }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = root.findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new ProductListAdapter(root.getContext(), mProductList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+
         return root;
     }
 }
