7563e072db1b2d63b49f3ee222d4927bd8528fed
Dan Hanegbi <hanegbidan@gmail.com>
2021-02-10 00:55:39 +0200

    Added simple Login and Register activities

--- app/src/main/java/com/comas/grush/LoginActivity.java
+++ app/src/main/java/com/comas/grush/LoginActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
