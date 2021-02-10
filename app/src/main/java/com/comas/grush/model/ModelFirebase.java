48aa44fda85c6820ceef786ca260eabd22194d72
kobi levi <kobilevi503@gmail.com>
2021-02-10 11:12:27 +0200

    Refactor ModelFirebase methods implementation

--- app/src/main/java/com/comas/grush/model/ModelFirebase.java
+++ app/src/main/java/com/comas/grush/model/ModelFirebase.java
@@ -3,48 +3,37 @@ package com.comas.grush.model;
 import android.util.Log;
 
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 public class ModelFirebase {
 
+    private static final String COLLECTION_PATH = "products";
+
     public void getAllProducts(Model.GetAllProductsListener listener) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("products").get()
-                .addOnCompleteListener(task -> {
-                    List<Product> products = new LinkedList<>();
-                    if (task.isSuccessful()) {
-                        Log.d("TAG", "Get all products SUCCESS");
-                        for (DocumentSnapshot doc : Objects.requireNonNull(task.getResult())) {
-                            Product product = doc.toObject(Product.class);
-                            products.add(product);
-                        }
-                    }
+        db.collection(COLLECTION_PATH).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    Log.d("TAG", "Get all products SUCCESS");
+                    List<Product> products = queryDocumentSnapshots.toObjects(Product.class);
                     listener.onComplete(products);
                 });
     }
 
     public void getProductById(String id, Model.GetProductByIdListener listener) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("products").document(id).get()
-                .addOnCompleteListener(task -> {
-                    Product product = null;
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot doc = task.getResult();
-                        product = doc.toObject(Product.class);
-                    }
+        db.collection(COLLECTION_PATH).document(id).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    Log.d("TAG", "Get a product SUCCESS");
+                    Product product = documentSnapshot.toObject(Product.class);
                     listener.onComplete(product);
                 });
     }
 
     public void addProduct(Product product, Model.AddProductListener listener) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference newProductRef = db.collection("products").document();
+        DocumentReference newProductRef = db.collection(COLLECTION_PATH).document();
         product.setId(newProductRef.getId());
         newProductRef.set(product)
                 .addOnSuccessListener(aVoid -> {
