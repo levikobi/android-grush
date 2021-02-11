8ff32feb03b1cec2d48e1d2423f091e56f80d31a
kobi levi <kobilevi503@gmail.com>
2021-02-11 16:44:15 +0200

    Add SwipeRefreshLayout

--- app/src/main/java/com/comas/grush/ui/home/HomeFragment.java
+++ app/src/main/java/com/comas/grush/ui/home/HomeFragment.java
@@ -1,34 +1,29 @@
 package com.comas.grush.ui.home;
 
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.comas.grush.R;
 import com.comas.grush.ui.product.ProductListAdapter;
-import com.comas.grush.model.Model;
-import com.comas.grush.model.Product;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel mViewModel;
 
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
     private ProductListAdapter mAdapter;
     private FloatingActionButton mAddProductFab;
@@ -37,6 +32,7 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         mViewModel = new ViewModelProvider((ViewModelStoreOwner) root.getContext()).get(HomeViewModel.class);
 
+        mSwipeRefreshLayout = root.findViewById(R.id.swipe_refresh);
         mRecyclerView = root.findViewById(R.id.recyclerview);
         mAddProductFab = root.findViewById(R.id.add_fab);
 
@@ -53,6 +49,15 @@ public class HomeFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
 
         mViewModel.refreshProductList(() -> mAdapter.notifyDataSetChanged());
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mViewModel.refreshProductList(() -> mAdapter.notifyDataSetChanged());
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
 
         mAddProductFab.setOnClickListener(view -> Navigation.findNavController(view)
                 .navigate(HomeFragmentDirections.actionHomeToProductCreate()));
