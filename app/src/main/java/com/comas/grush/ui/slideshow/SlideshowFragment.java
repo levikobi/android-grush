b5bbb0ebae0cd3e6310417ee931cfac3ef20f108
kobi levi <kobilevi503@gmail.com>
2021-02-15 14:10:55 +0200

    Update Authentication fragment UI

--- app/src/main/java/com/comas/grush/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/comas/grush/ui/slideshow/SlideshowFragment.java
@@ -31,11 +31,17 @@ public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
 
-    private FirebaseAuth mAuth;
+    private static int state;
+    private static final int REGISTER = 0;
+    private static final int LOGIN = 1;
 
+    private TextView mTitleTextView;
+    private EditText mNameEditText;
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
     private Button mSubmitButton;
+    private TextView mQuestionTextView;
+    private TextView mClickHereTextView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,62 +55,91 @@ public class SlideshowFragment extends Fragment {
     }
 
     private void initializeViewElements(View view) {
-        mAuth = FirebaseAuth.getInstance();
+        mTitleTextView = view.findViewById(R.id.auth_frag_title_textview);
+        mNameEditText = view.findViewById(R.id.auth_frag_name_edittext);
         mEmailEditText = view.findViewById(R.id.auth_frag_email_edittext);
         mPasswordEditText = view.findViewById(R.id.auth_frag_password_edittext);
         mSubmitButton = view.findViewById(R.id.auth_frag_submit_button);
+        mQuestionTextView = view.findViewById(R.id.auth_frag_question_textview);
+        mClickHereTextView = view.findViewById(R.id.auth_frag_clickhere_textview);
     }
 
     private void initializeViewHandlers() {
         mSubmitButton.setOnClickListener(this::handleSubmit);
+        mClickHereTextView.setOnClickListener(this::handleClickHere);
     }
 
     private void handleSubmit(View view) {
         String email = mEmailEditText.getText().toString();
         String password = mPasswordEditText.getText().toString();
 
-        NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
+        switch (state) {
+            case LOGIN:     login(view, email, password);       break;
+            case REGISTER:  register(view, email, password);    break;
+        }
+    }
 
-
+    private void login(View view, String email, String password) {
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
                         Log.d("TAG", "signInWithEmailAndPassword:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-
-
-                        Menu temp = navigationView.getMenu();
-                        MenuItem slideshow = temp.findItem(R.id.nav_slideshow);
-                        slideshow.setTitle("Logout");
-
-
-                        View headerView = navigationView.getHeaderView(0);
-                        TextView text = headerView.findViewById(R.id.nav_header_user_email);
-                        text.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
-
-                        slideshow.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                            @Override
-                            public boolean onMenuItemClick(MenuItem item) {
-                                FirebaseAuth.getInstance().signOut();
-                                slideshow.setTitle("Login");
-                                text.setText("Not logged in");
-
-
-                                return false;
-                            }
-                        });
-
+                        updateUI();
                         Navigation.findNavController(view).popBackStack();
-
-                        // updateUI(user);
                     } else {
-                        // If sign in fails, display a message to the user.
                         Log.w("TAG", "signInWithEmailAndPassword:failure", task.getException());
                         Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        // updateUI(null);
                     }
-                    // ...
                 });
+    }
+
+    private void register(View view, String email, String password) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d("TAG", "createUserWithEmail:success");
+                        updateUI();
+                        Navigation.findNavController(view).popBackStack();
+                    } else {
+                        Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void updateUI() {
+        NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
+        Menu temp = navigationView.getMenu();
+        MenuItem slideshow = temp.findItem(R.id.nav_slideshow);
+        slideshow.setTitle("Logout");
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView text = headerView.findViewById(R.id.nav_header_user_email);
+        text.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        slideshow.setOnMenuItemClickListener(item -> {
+            FirebaseAuth.getInstance().signOut();
+            slideshow.setTitle("Login");
+            text.setText("Not logged in");
+            return false;
+        });
+    }
+
+    private void handleClickHere(View view) {
+        switch (state) {
+            case REGISTER:
+                state = LOGIN;
+                mQuestionTextView.setText("Don't have an account? ");
+                mTitleTextView.setText("Login");
+                mNameEditText.setVisibility(View.GONE);
+                break;
+            case LOGIN:
+                state = REGISTER;
+                mQuestionTextView.setText("Already have an account? ");
+                mTitleTextView.setText("Register");
+                mNameEditText.setVisibility(View.VISIBLE);
+                break;
+            default: break;
+        }
     }
 }
