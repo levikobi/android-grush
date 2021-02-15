3b6bcfc12f7e1d9932c3c8f8d331aeca3e373b8a
Dan Hanegbi <hanegbidan@gmail.com>
2021-02-16 00:54:56 +0200

    Added icon, validation for auth, progress bars for auth

--- app/src/main/java/com/comas/grush/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/comas/grush/ui/slideshow/SlideshowFragment.java
@@ -1,6 +1,8 @@
 package com.comas.grush.ui.slideshow;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,6 +45,7 @@ public class SlideshowFragment extends Fragment {
     private Button mSubmitButton;
     private TextView mQuestionTextView;
     private TextView mClickHereTextView;
+    private ProgressBar mProgressBar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -62,6 +66,7 @@ public class SlideshowFragment extends Fragment {
         mSubmitButton = view.findViewById(R.id.auth_frag_submit_button);
         mQuestionTextView = view.findViewById(R.id.auth_frag_question_textview);
         mClickHereTextView = view.findViewById(R.id.auth_frag_clickhere_textview);
+        mProgressBar = view.findViewById(R.id.auth_frag_title_progressbar);
     }
 
     private void initializeViewHandlers() {
@@ -72,6 +77,25 @@ public class SlideshowFragment extends Fragment {
     private void handleSubmit(View view) {
         String email = mEmailEditText.getText().toString();
         String password = mPasswordEditText.getText().toString();
+        if (TextUtils.isEmpty(password)){
+            mPasswordEditText.setError("Password is Required.");    return;
+        }
+
+        if (TextUtils.isEmpty(email)){
+            mEmailEditText.setError("Email is Required.");  return;
+        }
+
+        if (TextUtils.isEmpty(email)){
+            mNameEditText.setError("Full Name is Required.");  return;
+        }
+
+        if (password.length() < 6){
+            mPasswordEditText.setError("Password must be at least 6 characters.");  return;
+        }
+
+        if (!email.contains("@")){
+            mEmailEditText.setError("Invalid email.");  return;
+        }
 
         switch (state) {
             case LOGIN:     login(view, email, password);       break;
@@ -80,20 +104,24 @@ public class SlideshowFragment extends Fragment {
     }
 
     private void login(View view, String email, String password) {
+        mProgressBar.setVisibility(View.VISIBLE);
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         Log.d("TAG", "signInWithEmailAndPassword:success");
+                        Toast.makeText(getContext(), "Logged in", Toast.LENGTH_SHORT).show();
                         updateUI();
                         Navigation.findNavController(view).popBackStack();
                     } else {
                         Log.w("TAG", "signInWithEmailAndPassword:failure", task.getException());
                         Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
+                    mProgressBar.setVisibility(View.INVISIBLE);
                 });
     }
 
     private void register(View view, String email, String password) {
+        mProgressBar.setVisibility(View.VISIBLE);
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
@@ -104,6 +132,8 @@ public class SlideshowFragment extends Fragment {
                         Log.w("TAG", "createUserWithEmail:failure", task.getException());
                         Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
+                    mProgressBar.setVisibility(View.INVISIBLE);
+
                 });
     }
 
@@ -111,16 +141,20 @@ public class SlideshowFragment extends Fragment {
         NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
         Menu temp = navigationView.getMenu();
         MenuItem slideshow = temp.findItem(R.id.nav_slideshow);
-        slideshow.setTitle("Logout");
-
+        slideshow.setTitle("Log Out");
         View headerView = navigationView.getHeaderView(0);
-        TextView text = headerView.findViewById(R.id.nav_header_user_email);
-        text.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        TextView userEmail = headerView.findViewById(R.id.nav_header_user_email);
+        userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        TextView userName = headerView.findViewById(R.id.nav_header_user_name);
+        String displayName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        userName.setText(displayName);
 
         slideshow.setOnMenuItemClickListener(item -> {
             FirebaseAuth.getInstance().signOut();
             slideshow.setTitle("Login");
-            text.setText("Not logged in");
+            userEmail.setText("Not logged in");
+            userName.setText("Anonymous");
             return false;
         });
     }
